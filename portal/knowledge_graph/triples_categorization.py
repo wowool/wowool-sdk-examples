70c1b3dbf17d4d07c13fd05ce4c43b26ce17eebd
Carolina Rubio <carolina@wowool.com>
2025-10-08 16:40:27 +0200

    fix typos and update code for pandas

--- portal/knowledge_graph/triples_categorization.py
+++ portal/knowledge_graph/triples_categorization.py
@@ -1,5 +1,4 @@
 from wowool.portal import Pipeline
-from wowool.entity_graph import CollectedResults
 from wowool.document import Document
 import pandas as pd
 
@@ -8,6 +7,7 @@ graph_options = {
     "nodes": {"_doc_": {"label": "Document", "name": "document.id"}},
     "themes": {"to": "_doc_"},
     "topics": {"to": "_doc_"},
+    "output_format": "table",
 }
 # Create the pipeline with English processing, syntax, entity recognition, and graph construction
 steps = [
@@ -23,7 +23,7 @@ steps = [
 documents = [
     {
         "id": "doc_finance.txt",
-        "text": "John Smith works on the stock exchange market . He recommends to by OpenAI stock.",
+        "text": "John Smith works on the stock exchange market . He recommends to buy OpenAI stock.",
     },
     {
         "id": "doc_sports.txt",
@@ -37,13 +37,11 @@ dfs = []
 for data in documents:
     doc = Document.create(data=data["text"], id=data["id"])
     doc = pipeline(doc)
-    links = doc.results("wowool_entity_graph")
-    merged_result = CollectedResults(links).merge()
-    df = pd.DataFrame(merged_result.rows)
+    df = pd.DataFrame(doc.entity_graph)
     dfs.append(df)
 
 df = pd.concat(dfs, ignore_index=True)
 # keep only the rows 'from_name' and 'to_name'
 df = df[["from_name", "to_name", "rel_label"]]
 
-print("DATAFRAME\n", df)
+print("Topics and themes per document\n", df)
